80c3dc85d8ef45421d1d25308b4fb7daa26a3072
axelmaxi2810 <axelmaxi2810@gmail.com>
2024-11-27 16:22:30 +0800

    add new backend

--- api/_controller/gateway.go
+++ api/_controller/gateway.go
@@ -5,15 +5,31 @@ import (
 	"encoding/json"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
 
+var backends = []struct {
+	URL  string
+	Code string
+}{
+	{"https://streambe01.indonesia.us.kg", "MAWAR"},
+	{"https://partial-maureene-zaxkyu-237ae0c6.koyeb.app", "MELATI"},
+}
+
 type GatewayController struct{}
 
+func getRandomBackend() (string, string) {
+	rand.Seed(time.Now().UnixNano())
+	backend := backends[rand.Intn(len(backends))]
+	return backend.URL, backend.Code
+}
+
 func (gc *GatewayController) Index(c *fiber.Ctx) error {
+	startTime := time.Now()
 	method := c.Method()
 	path := c.Path()
 	body := c.Body()
@@ -24,11 +40,11 @@ func (gc *GatewayController) Index(c *fiber.Ctx) error {
 	client := &http.Client{
 		Timeout: 500 * time.Second,
 	}
-	req, err := http.NewRequest(
-		method,
-		"https://streambe01.indonesia.us.kg"+path,
-		bytes.NewReader(body),
-	)
+
+	backendUrl, backendCode := getRandomBackend()
+	url := backendUrl + path
+
+	req, err := http.NewRequest(method, url, bytes.NewReader(body))
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"status":  fiber.StatusInternalServerError,
@@ -46,7 +62,7 @@ func (gc *GatewayController) Index(c *fiber.Ctx) error {
 	req.Header.Set("X-Gateway-Key", "6F1ED002AB5595859014EBF0951522D9")
 	req.Header.Set("Content-Type", "application/json")
 
-	log.Println(req.Header)
+	// log.Println(req.Header)
 
 	resp, err := client.Do(req)
 	if err != nil {
@@ -78,6 +94,19 @@ func (gc *GatewayController) Index(c *fiber.Ctx) error {
 			"message": "ERROR_JSON_RESPONSE",
 		})
 	}
-	log.Printf("Response: %s", resp.StatusCode)
+	executionTime := time.Since(startTime).Round(time.Second / 10)
+	jsonResponse["debug"] = fiber.Map{
+		"response_by":    backendCode,
+		"execution_time": executionTime.String(),
+	}
+
+	log.Printf(
+		"Response: [%d | %s | %s | %s]",
+		resp.StatusCode,
+		executionTime,
+		backendCode,
+		path,
+	)
+
 	return c.Status(int(resp.StatusCode)).JSON(jsonResponse)
 }
